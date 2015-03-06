7105033ad7696d6fb8e764eb8d61739ad984e3b1
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-03-06 22:39:18 +1000

    Split screen, placeholders, changing players, etc

---
+++ Trove of Thieves Unity/Assets/Scripts/s_CameraGUI.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class s_CameraGUI : MonoBehaviour {
+
+	public GameObject[] thieves;
+	public GameObject[] spawns;
+	public GameObject thief;
+	public GameObject blockOBJ;
+	public Vector3 blockSpawn;
+
+
+	public int playerTurn = 1; //0 = unit turn, 1 = p1, 2 = p2
+	public int lastTurn = 0; //What was playerTurns last state
+	public Text playerTurnUI;
+
+	void Start(){
+
+		//Spawn thieves at start of game
+		spawns = GameObject.FindGameObjectsWithTag ("ThiefSpawn");
+		foreach (GameObject spawn in spawns) {
+			Instantiate (thief, spawn.transform.position, Quaternion.identity);
+		}
+	}
+
+	void Update(){
+		playerTurnUI.text = "Current turn: Player " + playerTurn.ToString();
+	}
+
+	void OnGUI(){
+
+
+
+
+
+
+		//Buttons for testing
+		if (GUILayout.Button ("Move One Tile")){
+			DoMove();
+		}
+		if (GUILayout.Button ("Move Two Tiles")){
+			DoMove2();
+		}
+	}
+
+	void DoMove(){
+		thieves = GameObject.FindGameObjectsWithTag ("Thief");
+		
+		foreach (GameObject thief in thieves) {
+			thief.SendMessage ("DoMove", 1);
+		}
+	}
+	void DoMove2(){
+		thieves = GameObject.FindGameObjectsWithTag ("Thief");
+		
+		foreach (GameObject thief in thieves) {
+			thief.SendMessage ("DoMove", 2);
+		}
+	}
+
+	void EndTurn(){
+
+		int tempLastTurn = playerTurn; //this is to get who went last before it changes, but also to not change the actual last change int before using it below
+
+		//Get correct next turn (turns go 1,2,0,2,1,0,1,2,0,2,1 etc.etc.)
+		if (playerTurn == 0 && lastTurn == 1) {
+			playerTurn = 1;
+		} else if (playerTurn == 0 && lastTurn == 2){
+			playerTurn = 2;
+		} else if (playerTurn == 1 && lastTurn == 2){
+			playerTurn = 0;
+		} else if (playerTurn == 2 && lastTurn == 1){
+			playerTurn = 0;
+		} else if (playerTurn == 2 && lastTurn == 0){
+			playerTurn = 1;
+		} else if ((playerTurn == 1) && (lastTurn == 0)){
+			playerTurn = 2;
+		}
+
+		//change actual last turn
+		lastTurn = tempLastTurn; 
+	}
+}
---
+++ Trove of Thieves Unity/Assets/Scripts/s_Character.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_Character : MonoBehaviour {
+
+	Vector3 newPos;
+	bool canMove = true;
+
+	void Start () {
+		newPos = transform.position;
+	}
+
+	void Update () {
+		CollisionDetection ();
+		if (canMove) {
+			transform.position = Vector3.Lerp (transform.position, newPos, 0.02f);
+		}
+	}
+
+	void DoMove(int steps){
+		newPos = transform.position + -Vector3.left * steps / 3;
+	}
+
+	void CollisionDetection(){
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, -Vector3.left, out hit, 1)) {
+			if (hit.collider.tag == "Block"){
+				canMove = false;
+			} else {
+				canMove = true;
+			}
+		}
+	}
+}
